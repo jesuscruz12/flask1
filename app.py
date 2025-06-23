025efa6f6dfeb24a6fcb7207feba79054dfd9b59
jesuscruz12 <20221059@uthh.edu.mx>
2025-06-23 15:23:08 -0600

    actualizacion de los archivos

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
 
-app=Flask(__name__)
-@app.route('/')
+app = Flask(__name__)
+model = joblib.load("modelo_knn.pkl")
+
+@app.route("/")
 def home():
-    return 'Hello, World!'
-if __name__ == '__main__':
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        feats = np.array(data["features"], dtype=float).reshape(1, -1)
+
+        pred = model.predict(feats)[0]  # ya es 'saltamontes' o 'chicharra'
+        return jsonify({"prediction": pred})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
     app.run(debug=True)
